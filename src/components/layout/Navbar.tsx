6841e5a5b180b34f1e1a5198c3200ae9c711511f
neelaakhil12 <neelaakhil12@users.noreply.github.com>
2026-03-25 00:27:06 +0530

    Further optimize logo size using negative margins

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -84,13 +84,13 @@ export default function Navbar() {
           )}
 
           {/* Logo */}
-          <Link href="/" className="flex items-center p-0 m-0 leading-none z-10 transition-transform active:scale-95">
+          <Link href="/" className="flex items-center p-0 m-0 leading-none z-10 transition-transform active:scale-95 -my-2 md:-my-5">
             <Image 
               src="/madur-logo-official.png" 
               alt="MADUR.IN Logo" 
-              width={250}
-              height={80}
-              className="h-20 md:h-28 w-auto object-contain select-none mix-blend-multiply"
+              width={300}
+              height={100}
+              className="h-24 md:h-36 w-auto object-contain select-none mix-blend-multiply"
               priority
             />
           </Link>
